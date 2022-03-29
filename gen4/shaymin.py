d1a3e0489eba18f2cd08b9df5daee36a0a5e4938
niyrme <niyrme@niyrme.xyz>
2022-03-29 05:58:43 +0200

    update gen4.shaymin
    found out that you can just run from the battle and reload the area instead of doing a full game reset
    script is now almost 2x faster

--- gen4/shaymin.py
+++ gen4/shaymin.py
@@ -5,17 +5,16 @@ import numpy
 import serial
 
 import lib
+from lib import COLOR_BLACK
 from lib import COLOR_WHITE
 from lib import LOADING_SCREEN_POS
+from lib import PAD
 from lib import ReturnCode
-from lib.gen4 import awaitInGameSpam
 from lib.gen4 import ENCOUNTER_DIALOG_POS
+from lib.gen4 import OWN_POKEMON_POS
 
 
 def _main(ser: serial.Serial, vid: cv2.VideoCapture, e: int, **kwargs) -> tuple[int, ReturnCode, numpy.ndarray]:
-	lib.resetGame(ser, vid)
-	awaitInGameSpam(ser, vid)
-
 	lib.press(ser, vid, "A")
 	lib.waitAndRender(vid, 3)
 	lib.press(ser, vid, "A")
@@ -26,7 +25,37 @@ def _main(ser: serial.Serial, vid: cv2.VideoCapture, e: int, **kwargs) -> tuple[
 
 	# encounter dialog
 	rc, encounterFrame = lib.checkShinyDialog(ser, vid, ENCOUNTER_DIALOG_POS, COLOR_WHITE, 1.5)
-	return (e + 1, rc, encounterFrame)
+	if rc == ReturnCode.SHINY:
+		return (e + 1, ReturnCode.SHINY, encounterFrame)
+
+	# wait until UI is done loading
+	lib.awaitPixel(ser, vid, pos=OWN_POKEMON_POS, pixel=COLOR_WHITE)
+	lib.waitAndRender(vid, 1)
+
+	while True:
+		lib.press(ser, vid, "w")
+		lib.waitAndRender(vid, 0.5)
+		lib.press(ser, vid, "A")
+		lib.waitAndRender(vid, 1)
+		lib.press(ser, vid, "B")
+
+		if lib.awaitPixel(ser, vid, pos=OWN_POKEMON_POS, pixel=COLOR_BLACK, timeout=10):
+			print("fade out", PAD)
+			break
+		else:
+			lib.waitAndRender(vid, 10)
+
+	lib.awaitNotPixel(ser, vid, pos=OWN_POKEMON_POS, pixel=COLOR_BLACK)
+	print("return to game")
+	lib.waitAndRender(vid, 1)
+
+	lib.press(ser, vid, "A")
+	lib.waitAndRender(vid, 0.5)
+
+	lib.press(ser, vid, "s", 3.5)
+	lib.waitAndRender(vid, 0.2)
+	lib.press(ser, vid, "w", 3.8)
+	return (e + 1, ReturnCode.OK, encounterFrame)
 
 
 if __name__ == "__main__":
