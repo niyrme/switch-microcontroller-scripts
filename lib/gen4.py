a6be5c0cf97cc60dd7189a8d58128eb1baf1d8dd
niyrme <niyrme@niyrme.xyz>
2022-04-10 22:44:16 +0200

    update lib
    add Gen4Script subclass of lib.Script

--- lib/gen4.py
+++ lib/gen4.py
@@ -1,70 +1,70 @@
-import cv2
 import numpy
-import serial
 
-import lib
 from lib import COLOR_BLACK
 from lib import LOADING_SCREEN_POS
 from lib import PAD
 from lib import Pos
+from lib import RunCrash
+from lib import Script
 
 
 ENCOUNTER_DIALOG_POS = Pos(670, 430)
+SHORT_DIALOG_POS = Pos(560, 455)
 OWN_POKEMON_POS = Pos(5, 425)
 
 
-def awaitInGame(ser: serial.Serial, vid: cv2.VideoCapture) -> None:
-	crashed = not lib.awaitPixel(ser, vid, pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
-	print("startup screen", PAD)
+class Gen4Script(Script):
+	def awaitInGame(self) -> None:
+		crashed = not self.awaitPixel(pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
+		print("startup screen", PAD)
 
-	crashed |= not lib.awaitNotPixel(ser, vid, pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
-	print("after startup", PAD)
+		crashed |= not self.awaitNotPixel(pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
+		print("after startup", PAD)
 
-	if crashed is True:
-		raise lib.RunCrash
+		if crashed is True:
+			raise RunCrash
 
-	# in splash screen
-	lib.waitAndRender(vid, 1)
-	lib.press(ser, vid, "A")
-	lib.waitAndRender(vid, 3)
-	lib.press(ser, vid, "A")
-	lib.waitAndRender(vid, 3)
+		# in splash screen
+		self.waitAndRender(1)
+		self.press("A")
+		self.waitAndRender(3)
+		self.press("A")
+		self.waitAndRender(3)
 
-	# loading screen to game
-	crashed |= not lib.awaitPixel(ser, vid, pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
-	print("loading screen", PAD)
-	crashed |= not lib.awaitNotPixel(ser, vid, pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
+		# loading screen to game
+		crashed |= not self.awaitPixel(pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
+		print("loading screen", PAD)
+		crashed |= not self.awaitNotPixel(pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
 
-	if crashed is True:
-		raise lib.RunCrash
+		if crashed is True:
+			raise RunCrash
 
-	print("in game", PAD)
-	lib.waitAndRender(vid, 1)
+		print("in game", PAD)
+		self.waitAndRender(1)
 
+	def awaitInGameSpam(self) -> None:
+		self.awaitPixel(pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
+		print("startup screen", PAD)
 
-def awaitInGameSpam(ser: serial.Serial, vid: cv2.VideoCapture) -> None:
-	lib.awaitPixel(ser, vid, pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
-	print("startup screen", PAD)
+		self.whilePixel(LOADING_SCREEN_POS, COLOR_BLACK, 0.5, lambda: self.press("A"))
+		print("after startup", PAD)
 
-	lib.whilePixel(ser, vid, LOADING_SCREEN_POS, COLOR_BLACK, 0.5, lambda: lib.press(ser, vid, "A"))
-	print("after startup", PAD)
+		self.waitAndRender(1)
 
-	lib.waitAndRender(vid, 1)
+		frame = self.getframe()
+		if numpy.array_equal(frame[LOADING_SCREEN_POS.y][LOADING_SCREEN_POS.x], (41, 41, 41)):
+			raise RunCrash
 
-	frame = lib.getframe(vid)
-	if numpy.array_equal(frame[LOADING_SCREEN_POS.y][LOADING_SCREEN_POS.x], (41, 41, 41)):
-		raise lib.RunCrash
+		self.press("A")
+		self.waitAndRender(3)
 
-	lib.press(ser, vid, "A")
-	lib.waitAndRender(vid, 3)
+		# loading screen to game
+		crashed = not self.awaitPixel(pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
+		print("loading screen", PAD)
+		crashed |= not self.awaitNotPixel(pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
 
-	# loading screen to game
-	crashed = not lib.awaitPixel(ser, vid, pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
-	print("loading screen", PAD)
-	crashed |= not lib.awaitNotPixel(ser, vid, pos=LOADING_SCREEN_POS, pixel=COLOR_BLACK)
+		if crashed is True:
+			raise RunCrash
 
-	if crashed is True:
-		raise lib.RunCrash
-
-	print("in game", PAD)
-	lib.waitAndRender(vid, 1)
+		print("in game", PAD)
+		self.waitAndRender(1)
